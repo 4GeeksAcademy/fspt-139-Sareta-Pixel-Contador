1f97e545ebb8d244079c9e77610173b4c350ecd5
SaraMarti <saramartialcocel@gmail.com>
2026-07-07 11:32:21 +0000

    contador

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -3,25 +3,36 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+//create your first component
+const Home = ({ hoursStr, minutesStr, secondsStr, onStop}) => {
+	let inputValue = 0
+	return (
+		<>
+			<div className="intro">
+				<h1>Soy un contador simplón</h1>
+			</div>
+			<div className="contador">
+				<div className="icon"><i className="fa-regular fa-clock"></i></div>
+				<div className="hours">
+					<div className="firstDigit">{hoursStr[0]}</div>
+					<div className="secondDigit">{hoursStr[1]}</div>:
+				</div>
+				<div className="minutes">
+					<div className="firstDigit">{minutesStr[0]}</div>
+					<div className="secondDigit">{minutesStr[1]}</div>:
+				</div>
+				<div className="seconds">
+					<div className="firstDigit">{secondsStr[0]}</div>
+					<div className="secondDigit">{secondsStr[1]}</div>
+				</div>
+
+			</div>
+			<div className="boton" aria-label="Ejemplo básico con contorno">
+				<button type="button" className="btn btn-outline-primary"onClick={onStop}>Pause</button>
+			</div>
+
+		</>
 	);
 };
 
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -5,14 +5,48 @@ import ReactDOM from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
 
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 // index.css'
 import '../styles/index.css'
 
 // components
 import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+let seconds = 0
+let minutes = 0
+let hours = 0
+
+let interval = setInterval(() => {
+  
+  seconds++
+  if (seconds == 59){
+    seconds = 0
+    minutes++
+  }
+  if (minutes == 59){
+    minutes = 0
+    hours++
+  }
+  let secondsStr =String(seconds).padStart(2, "0")
+  let minutesStr =String(minutes).padStart(2,"0")
+  let hoursStr = String(hours).padStart(2,"0")
+  root.render(
+    <React.StrictMode>
+      <Home secondsStr= {secondsStr} minutesStr= {minutesStr} hoursStr={hoursStr} onStop={onStop}></Home>
+    </React.StrictMode>,
+
+  )
+}, 1000)
+
+const onStop = () => {
+  clearInterval(interval)
+}
+
+
+
+
+
